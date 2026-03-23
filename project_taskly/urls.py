f461c8c7e9ae06b3f3be0f0c1ac1e05bdfcd3668
samitshah01 <samitshah49@gmail.com>
2026-03-24 00:13:27 +0545

    feat: update URL patterns

--- project_taskly/urls.py
+++ project_taskly/urls.py
@@ -3,13 +3,18 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('privacy/', views.privacy, name='privacy'),
-    path('terms/', views.terms, name='terms'),
     path('login/', views.login, name='login'),
+    path('check-username/', views.check_username, name='check_username'),
+    
     path('forgot-password/', views.forgot_password, name='forgot_password'),
+    path('verify-otp/', views.verify_otp_view, name='verify_otp'),
+    path('resend-otp/', views.resend_otp, name='resend_otp'),
+    path('reset-password/', views.reset_password, name='reset_password'),
+    
     path('register/', views.register, name='register'),
-    path('logout/', views.logout, name='logout'),
+    
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('logout/', views.logout, name='logout'),
     path("projects/", views.projects, name="projects"),
     path("projects/create/", views.create_project, name="create_project"),
     path("projects/view/<int:id>/", views.view_project, name="view_project"),
