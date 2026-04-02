c45d2e9482507f8edd3ddb11a605694341c68532
samitshah01 <samitshah49@gmail.com>
2026-04-02 20:18:11 +0545

    feat: add devtools dummy endpoint

--- taskly/urls.py
+++ taskly/urls.py
@@ -4,8 +4,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from django.http import JsonResponse
+
+def devtools_dummy(request):
+    return JsonResponse({}, status=204)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('project_taskly.urls')),
+    path('.well-known/appspecific/com.chrome.devtools.json', devtools_dummy),
 ]
